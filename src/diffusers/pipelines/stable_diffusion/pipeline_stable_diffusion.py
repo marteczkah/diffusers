d2256cd4ca7b26f593b4e69de647c607e9ac84bf
Marta <48037981+marteczkah@users.noreply.github.com>
2023-11-11 19:41:20 -0500

    Update pipeline_stable_diffusion.py

--- src/diffusers/pipelines/stable_diffusion/pipeline_stable_diffusion.py
+++ src/diffusers/pipelines/stable_diffusion/pipeline_stable_diffusion.py
@@ -661,6 +661,7 @@ class StableDiffusionPipeline(DiffusionPipeline, TextualInversionLoaderMixin, Lo
             generator,
             latents,
         )
+        print(latents.mean())
 
         # 6. Prepare extra step kwargs. TODO: Logic should ideally just be moved out of the pipeline
         extra_step_kwargs = self.prepare_extra_step_kwargs(generator, eta)
